9898a4a37c5d51bb078d5c1bb787869de3eca556
marjan <marjan.jangholi@yahoo.com>
2020-12-27 22:32:43 +0330

    use ObjectsBox and TeeterTottle component in js

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <div id="app">
-    <p>hh</p>
+  <div id="app" class="gameArea">
+    <ObjectsBox />
+    <TeeterTottle />
   </div>
 </template>
 
 <script>
+import ObjectsBox from './components/ObjectsBox'
+import TeeterTottle from './components/TeeterTottle'
 
 export default {
   name: 'App',
   components: {
-
+    ObjectsBox,
+    TeeterTottle
   }
 }
 </script>
+<style scoped>
+  .gameArea {
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    padding-top: 1rem;
+  }
+</style>
