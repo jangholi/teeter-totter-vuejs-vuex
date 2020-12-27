0a6db93896925f6f1026b4fdd35fc962f079f1be
marjan <marjan.jangholi@yahoo.com>
2020-12-28 01:03:32 +0330

    install and use font awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store/store';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faPause, faPlay } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faPause, faPlay);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false
 
