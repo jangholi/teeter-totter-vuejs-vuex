aa207d144450281e240443ed476b12e3e90f1751
marjan <marjan.jangholi@yahoo.com>
2020-12-27 18:32:23 +0330

    add constants

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const TEETER_TOTTER_WIDTH = 10;
+export const MAX_WEIGHT = 10;
+export const MIN_WEIGHT = 1;
+export const MAX_BENDING = 30;
+export const MIN_BENDING = -30;
+export const MAX_SIDES_DIFFERENCE = 20;
