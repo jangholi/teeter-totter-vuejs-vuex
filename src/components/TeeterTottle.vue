2d5e30054f8626d685b80e23fd9c1068d5c528b4
marjan <marjan.jangholi@yahoo.com>
2020-12-27 22:33:24 +0330

    create teeter totter component with randomly add right object

---
+++ src/components/TeeterTottle.vue
@@ -0,0 +1,72 @@
+<template>
+    <section class="teeterTottle">
+        <div class="swing" :style="swingInlineStyle">
+            <Block v-for="block in leftSideObjects"
+                   :block="block"
+                   :key="block.id"/>
+            <Block v-for="block in rightSideObjects"
+                   :block="block"
+                   :side="true"
+                   :key="block.id"/>
+        </div>
+        <div class="pillar"></div>
+    </section>
+</template>
+
+<script>
+    import Block from './Block'
+    import {mapState, mapMutations} from 'vuex'
+
+    export default {
+        name: "TeeterTottle",
+        components: {
+            Block
+        },
+        beforeMount() {
+            this.addObjectToRight()
+        },
+        methods: {
+            ...mapMutations({
+                addObjectToRight: 'addObjectToRight'
+            })
+        },
+        computed: {
+            ...mapState([
+                'rightSideObjects',
+                'leftSideObjects'
+            ]),
+            swingBendingDegree() {
+                return this.$store.getters.swingBendingDegree
+            },
+            swingInlineStyle() {
+                return {
+                    transform: `rotate(${this.swingBendingDegree / 2}deg)`
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .swing {
+        width: 100%;
+        height: 0.5rem;
+        background-color: #263238;
+        position: relative;
+        transform: rotate(0deg);
+        transition: transform 0.4s ease-in-out;
+    }
+    .pillar {
+        width: 0;
+        height: 0;
+        margin: 0 auto;
+        border-style: solid;
+        border-width: 0 75px 150px 75px;
+        border-color: transparent transparent #263238 transparent;
+    }
+    .teeterTottle{
+        height: 35%;
+        width: 100%;
+        margin-top: 300px;
+    }
+</style>
